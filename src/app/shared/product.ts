59310a0b9efebf3d08d821c4c95fb595a0114f5c
Marcelo Martins <m.a.martins@live.com>
2019-08-19 18:16:03 -0300

    Changed to interface

--- src/app/shared/product.ts
+++ src/app/shared/product.ts
@@ -1,11 +1,8 @@
-export class Product {
-  constructor(
-    public id: number,
-    public title: string,
-    public price: number,
-    public rating: number,
-    public description: string,
-    public categories: string[]
-  ) {
-  }
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  description: string;
+  categories: string[];
 }
